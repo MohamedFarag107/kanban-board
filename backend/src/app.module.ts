b2797a649ad7de46037ae475032a5db1a5a2c6c6
Mohamed Farag Mohamed <mo.farag.dev@gmail.com>
2024-10-25 10:49:06 +0300

    feat[backend]: add different environment (development - production) for the app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
 import { Member } from './members/member.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.development', '.env.production'],
+      envFilePath: `.env.${process.env.NODE_ENV}`,
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
@@ -18,20 +18,17 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER_NAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         entities: [Member],
-        synchronize:
-          configService.get<'development' | 'production'>('NODE_ENV') ===
-          'development',
+        synchronize: isDevelopment,
+        ssl: true,
       }),
       inject: [ConfigService],
     }),
     MembersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
